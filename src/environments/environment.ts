7978c8d8faaa7bd492f85566ff614a9b6ad05b9e
chinmayshringi4@gmail.com <chinmayshringi4@gmail.com>
2021-04-28 20:07:52 +0530

    add key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,7 @@
 export const environment = {
   production: false,
   firebaseConfig : {
-    apiKey: "AIzaSyDUNoE7Mc11KL1Swjyawcg-P3xYFQdMfPk",
-    authDomain: "termitefabrications.firebaseapp.com",
-    projectId: "termitefabrications",
-    storageBucket: "termitefabrications.appspot.com",
-    messagingSenderId: "165482160280",
-    appId: "1:165482160280:web:8a6dfbf0720079566bb0ed",
-    measurementId: "G-8E28QZHZHD"
+    // ADD HERE
   }
 };
 
